467597ae0ce52dffddb39e1595486a77ff9eafe8
Nikolay Rys <nikolay@rys.me>
2021-09-06 19:26:30 +0300

    Db access snippets

---
+++ db_snippets.rb
@@ -0,0 +1,17 @@
+require 'sqlite3'
+db = SQLite3::Database.new('dataset.db')
+
+db.execute <<-SQL
+  SELECT id, passed_days, (target_score - current_score) AS change 
+  FROM posts 
+  ORDER BY change DESC LIMIT 100
+SQL
+
+
+db.execute <<-SQL
+  SELECT COUNT(*)
+  FROM posts
+SQL
+
+
+
